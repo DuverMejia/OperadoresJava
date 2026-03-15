82cfa971a63848c965b2e94f1bf9ebafd433d537
unknown <dowerx96@hotmail.es>
2026-03-15 11:48:01 -0500

    se agrega archivo con operadores de asignación

---
+++ Operadores/src/OperadoresDeAsignacion.java
@@ -0,0 +1,14 @@
+public class OperadoresDeAsignacion {
+    static void main(String[] args) {
+        var miNumero = 10;
+        int miNumero2;
+        miNumero2 = 5;
+        //operador compuesto
+        miNumero += 10;
+        System.out.println("miNumero = " + miNumero);
+        // tenemos *= , /=, >=, <=
+        miNumero *= 2;
+        System.out.println("miNumero = " + miNumero);
+
+    }
+}
