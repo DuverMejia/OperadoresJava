be74b34d0fc12e2ff2cef173e69c00852d80e428
unknown <dowerx96@hotmail.es>
2026-03-15 12:14:51 -0500

    se agrega ejecicio valor rango

--- Operadores/src/EjercicioRango.java
+++ Operadores/src/EjercicioRango.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class EjercicioRango {
+    static void main(String[] args) {
+        //ejercicio dentro de rango si esta entre 0 y 5
+        Scanner console = new Scanner(System.in);
+        int a;
+        System.out.print("Escriba un número entre 0 y 5: ");
+        a = Integer.parseInt(console.nextLine());
+        boolean resultado;
+        resultado = a >= 0 && a <= 5;
+        System.out.println("el número proporcionado esta en el rango = " + resultado);
+    }
 }
