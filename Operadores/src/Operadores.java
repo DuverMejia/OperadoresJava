0df297df31574ffd4373bd4e2a0483a904397150
unknown <dowerx96@hotmail.es>
2026-03-15 09:36:28 -0500

    Se agrega archivo donde se prueban operadores aritmeticos

--- Operadores/src/Operadores.java
+++ Operadores/src/Operadores.java
@@ -1,2 +1,26 @@
 public class Operadores {
+    static void main() {
+        //operadores aritmeticos
+        int a = 3, b= 2, resultado = 0;
+        //suma
+        resultado = a+ b ;
+        System.out.println("suma = " + resultado);
+        //Resta
+        resultado = a-b ;
+        System.out.println("resta = " + resultado);
+        
+        //multiplicacion
+        resultado = a*b ;
+        System.out.println("multiplicación = " + resultado);
+
+        //Dvivisión
+        resultado = a/b ;
+        System.out.println("División = " + resultado);
+
+        //modulo
+
+        resultado = a%b ;
+        System.out.println("modulo = " + resultado);
+
+    }
 }
