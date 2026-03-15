be810358e66300543436c25459e2ec037727316e
unknown <dowerx96@hotmail.es>
2026-03-15 11:36:42 -0500

    Se agrega ejemplo operadores logicos de comparación

---
+++ Operadores/src/OperadoresLogicos.java
@@ -0,0 +1,21 @@
+public class OperadoresLogicos {
+    static void main(String[] args) {
+    //Operadores logicos
+        boolean a = true, b = false, d = false, c = true, resultado;
+
+                //operador and
+        resultado = a & b ;
+        System.out.println("resultado = " + resultado);
+
+        //operador or
+
+        resultado = b || d ;
+        System.out.println("resultado = " + resultado);
+
+        //operador not
+
+        resultado = !resultado;
+        System.out.println("resultado = " + resultado);
+
+    }
+}
