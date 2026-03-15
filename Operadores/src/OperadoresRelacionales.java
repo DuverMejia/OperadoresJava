fcd03b39f7c371de893d360b90dc701fc718dce8
unknown <dowerx96@hotmail.es>
2026-03-15 10:37:54 -0500

    se agregan operadores relacionales

--- Operadores/src/OperadoresRelacionales.java
+++ Operadores/src/OperadoresRelacionales.java
@@ -1,3 +1,31 @@
 public class OperadoresRelacionales {
+    static void main() {
+        //operadores relacionales/comparacion
+        int  a = 3, b = 2, c =3;
 
+        //igualdad
+        boolean  resultado = a ==b;
+        System.out.println("resultado = " + resultado);
+
+        resultado = a==c;
+        System.out.println("resultado = " + resultado);
+
+        //operador distinto
+        resultado = a !=b;
+        System.out.println("resultado = " + resultado);
+
+        //mayor que
+        resultado = a > b;
+        System.out.println("resultado = " + resultado);
+        resultado = a > c ;
+        System.out.println("resultado = " + resultado);
+        resultado = a >=b;
+        System.out.println("resultado = " + resultado);
+
+        //menor que
+        resultado = a < b;
+        System.out.println("resultado = " + resultado);
+        resultado = a <= b ;
+        System.out.println("resultado = " + resultado);
+    }
 }
